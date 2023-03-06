1ea42fe3e9a2bed8c19f54e0442880f7bbbf7418
kunfirekun <kunfirekun@gmail.com>
2023-03-06 17:13:53 +0300

    added class method addaccount() and exception handling of the inputs

--- account_class.php
+++ account_class.php
@@ -29,3 +29,57 @@ class Account
 		
 	}
 }
+/* Add a new account to the system and return its ID (the account_id column of the accounts table) */
+public function addAccount(string $name, string $passwd): int
+{
+	/* Global $pdo object */
+	global $pdo;
+	
+	/* Trim the strings to remove extra spaces */
+	$name = trim($name);
+	$passwd = trim($passwd);
+	
+	/* Check if the user name is valid. If not, throw an exception */
+	if (!$this->isNameValid($name))
+	{
+		throw new Exception('Invalid user name');
+	}
+	
+	/* Check if the password is valid. If not, throw an exception */
+	if (!$this->isPasswdValid($passwd))
+	{
+		throw new Exception('Invalid password');
+	}
+	
+	/* Check if an account having the same name already exists. If it does, throw an exception */
+	if (!is_null($this->getIdFromName($name)))
+	{
+		throw new Exception('User name not available');
+	}
+	
+	/* Finally, add the new account */
+	
+	/* Insert query template */
+	$query = 'INSERT INTO myschema.accounts (account_name, account_passwd) VALUES (:name, :passwd)';
+	
+	/* Password hash */
+	$hash = password_hash($passwd, PASSWORD_DEFAULT);
+	
+	/* Values array for PDO */
+	$values = array(':name' => $name, ':passwd' => $hash);
+	
+	/* Execute the query */
+	try
+	{
+		$res = $pdo->prepare($query);
+		$res->execute($values);
+	}
+	catch (PDOException $e)
+	{
+	   /* If there is a PDO exception, throw a standard exception */
+	   throw new Exception('Database query error');
+	}
+	
+	/* Return the new ID */
+	return $pdo->lastInsertId();
+}
