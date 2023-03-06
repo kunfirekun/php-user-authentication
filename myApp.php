d8666600e6faa9a3c2abdea8b8e6d9420c6b583f
kunfirekun <kunfirekun@gmail.com>
2023-03-06 17:21:19 +0300

    added how to use the addaccount method

--- myApp.php
+++ myApp.php
@@ -5,3 +5,16 @@ session_start();
 require './db_inc.php';
 //crud functions file
 require './account_class.php';
+//how to use addcount method 
+$account = new Account();
+try
+{
+	$newId = $account->addAccount('myNewName', 'myPassword');
+}
+catch (Exception $e)
+{
+	/* Something went wrong: echo the exception message and die */
+	echo $e->getMessage();
+	die();
+}
+echo 'The new account ID is ' . $newId;
