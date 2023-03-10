24f5c728e8b622f7fda376537a6d4cbd611b2fbc
kunfirekun <kunfirekun@gmail.com>
2023-03-10 16:08:03 +0300

    index file edited to allow username and ip address input from the function in the engine file edited

--- index.php
+++ index.php
@@ -1,15 +1,28 @@
 <?php
 
 require 'config.php';
+require 'engine.php';
 
 try {
 	$pdo = new PDO($dsn, $user, $password);
 
 	if ($pdo) {
-		echo "Connected to the $db database successfully!";
+        // insert a single publisher
+        $name = 'Macmillan';
+        //obtained from engine ipaddress function
+        $ip_address= getUserIpAddr();
+        //sql query to insert
+        $sql = 'INSERT INTO the_numbers(username,ip_address) VALUES(:name,:ip_address)';
+        //prepared statement for the sql query
+        $statement = $pdo->prepare($sql);
 
+        $statement->execute([
+            ':name' => $name,
+            ':ip_address' => $ip_address
+        ]);
 
-
+        //output
+		echo "The username $name and $ip_address entered to database successfully!";
 
 
 	}
