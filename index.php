e562e2656f451b48fe5ba06f1457e558506113a2
kunfirekun <kunfirekun@gmail.com>
2023-03-10 15:57:47 +0300

    successful connect to database template

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
-//database connection
-require_once "config.php";
+
+require 'config.php';
+
+try {
+	$pdo = new PDO($dsn, $user, $password);
+
+	if ($pdo) {
+		echo "Connected to the $db database successfully!";
 
 
 
-echo 'User Real IP - '.getUserIpAddr();
-?>
+
+
+	}
+} catch (PDOException $e) {
+	echo $e->getMessage();
+}
